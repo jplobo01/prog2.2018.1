f2d8de30db132936b8eb2f90e3cc0279c1ce0a98
jplobo01 <jpbarbosa18@gmail.com>
2018-02-19 20:57:43 -0300

    Add files via upload

---
+++ aula1Prog2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main();
+{
+	int numeros[10];
+	int maior=0, i;
+	
+	for(i=0;i<10;i++)
+	{
+		printf("\n Entre com o numero:");
+		scanf("%d", &numeros);
+		if(numeros)[i]>maior)
+		{
+			maior = numeros[i];
+		}
+	//exbir na tela o maior valor lido
+	printf("\n O maior valor=%d", maior);
+	
+	return 0;
+	}
+}
